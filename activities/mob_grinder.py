159a7dde4edd436d139e12949b1bea5303a42d58
Artur K <artur.aklimek@gmail.com>
2023-12-21 13:23:52 +0100

    change atack speed

--- activities/mob_grinder.py
+++ activities/mob_grinder.py
@@ -35,8 +35,8 @@ def set_is_running_grind_procedure(new_state: bool = False) -> None:
 
 attacks_number_in_iteration = 20
 
-min_atack_interval = 0.5
-max_atack_interval = 3
+min_atack_interval = 0.4
+max_atack_interval = 1.5
 
 sword_slot = 1
 
